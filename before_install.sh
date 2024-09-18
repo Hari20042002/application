acc02ee5a5baddb697c46f363fb2dc6abad7c918
Hari20042002 <113891115+Hari20042002@users.noreply.github.com>
2024-09-18 21:19:10 +0530

    Update before_install.sh

--- before_install.sh
+++ before_install.sh
@@ -2,6 +2,9 @@
 
 # Exit immediately if a command exits with a non-zero status
 set -e
+# Assigning ownership permission
+sudo chown testuser:testuser /var/www/html/testdir/application/*
+
 
 # Remove existing application files and directories
 rm -rf /var/www/html/testdir/application/*
