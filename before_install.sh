b1658748aa54bec0b7c99fa92c2f111da56cffde
Hari20042002 <113891115+Hari20042002@users.noreply.github.com>
2024-09-18 21:23:17 +0530

    Update before_install.sh

--- before_install.sh
+++ before_install.sh
@@ -3,7 +3,7 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 # Assigning ownership permission
-sudo chown testuser:testuser /var/www/html/testdir/application/*
+chown testuser:testuser /var/www/html/testdir/application/*
 
 
 # Remove existing application files and directories
